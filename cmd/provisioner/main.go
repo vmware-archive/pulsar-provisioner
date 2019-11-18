2b1490e8fd9e0f79f1229c648477f01b067d0254
Eric Bottard <ebottard@pivotal.io>
2019-11-18 21:13:18 +0100

    Doh!

---
+++ cmd/provisioner/main.go
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019 The original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	gateway := os.Getenv("GATEWAY")
+	if gateway == "" {
+		log.Fatal("Environment variable GATEWAY should contain the host and port of a liiklus gRPC endpoint")
+	}
+	broker := os.Getenv("BROKER")
+	if broker == "" {
+		log.Fatal("Environment variable BROKER should contain the service URL of a Pulsar cluster")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		handleProvisionRequest(broker, gateway, w, r)
+	})
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func handleProvisionRequest(broker, gateway string, writer http.ResponseWriter, request *http.Request) {
+
+	parts := strings.Split(request.URL.Path[1:], "/")
+	if len(parts) != 2 {
+		writer.WriteHeader(http.StatusBadRequest)
+		_, _ = fmt.Fprintf(writer, "URLs should be of the form /<namespace>/<stream-name>\n")
+		return
+	}
+	// TODO: support configurable tenant other than "public"
+	topicName := fmt.Sprintf("persistent://public/%s/%s", parts[0], parts[1])
+
+	writer.WriteHeader(http.StatusOK)
+
+	if err := encodeResponse(writer, gateway, topicName); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to write json response: %v", err)
+		return
+	}
+	_, _ = fmt.Fprintf(os.Stderr, "Reported successful topic %q\n", topicName)
+
+}
+
+func encodeResponse(w http.ResponseWriter, gateway string, topicName string) error {
+	w.Header().Set("Content-Type", "application/json")
+	res := result{
+		Gateway: gateway,
+		Topic:   topicName,
+	}
+	return json.NewEncoder(w).Encode(res)
+}
+
+type result struct {
+	Gateway string `json:"gateway"`
+	Topic   string `json:"topic"`
+}
