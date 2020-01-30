13d4eedf64f435e3f20eb7475c501bd9548f8377
Scott Andrews <sandrews@pivotal.io>
2020-01-30 00:02:27 -0500

    Test PRs
    - test PRs
    - test and publish pushes
    - avoid rebuilding docker image, retag existing image instead
      - tag pushes on master as :latest
    - bump go to 1.13

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -8,11 +8,24 @@ gcloud config set disable_prompts True
 gcloud auth activate-service-account --key-file <(echo ${GCLOUD_CLIENT_SECRET} | base64 --decode)
 gcloud auth configure-docker
 
-readonly root=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)
 readonly version=$(cat ${root}/VERSION)
 readonly git_branch=${GITHUB_REF:11} # drop 'refs/head/' prefix
 readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
 readonly slug=${version}-${git_timestamp}-${GITHUB_SHA:0:16}
 
+publishImage() {
+  local tag=$1
+  local source=ko.local/provisioner-46159645f685fedb8f6279549d9d9574:latest
+  local destination=gcr.io/projectriff/pulsar-provisioner/provisioner:${tag}
+
+  docker tag ${source} ${destination}
+  docker push ${destination}
+}
+
 echo "Publishing riff pulsar provisioner"
-(cd $root && KO_DOCKER_REPO="gcr.io/projectriff/pulsar-provisioner" ko publish -t "${version}" -t "${slug}" github.com/projectriff/pulsar-provisioner/cmd/provisioner)
+
+publishImage ${slug}
+publishImage ${vesion}
+if [ ${git_branch} = master ] ; then
+  publishImage latest
+fi
